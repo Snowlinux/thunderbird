84f34e83a7fd626f403f5f483632fe2412745b78
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-11-22 15:56:29 +0000

    Fixes

--- update.sh
+++ update.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-ARCHI=`getconf LONG_BIT`
-if [[ $ARCHI == *64* ]]; then
- ARCHI="amd64"
+ARCHI=$1
+CURDIR=$2
+if [[ $ARCHI == *amd64* ]]; then
  ARCH_URL="x86_64"
 else
- ARCHI="i386"
  ARCH_URL="i686"
 fi
 
-mkdir -p opt
-rm -rf opt/*
-cd opt
-wget --quiet -nd -r -l1 --no-parent -A "*.bz2" http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/latest/linux-$ARCH_URL/en-US/
-
-RELEASE=`ls thunderbird* | cut -d "-" -f2 | sed "s/.tar.bz2//"`
-bzip2 -d thunderbird-$RELEASE.tar.bz2
-tar xf thunderbird-$RELEASE.tar
-rm thunderbird-$RELEASE.tar
+echo "Downloading Thunderbird for $ARCHI"
+cd $CURDIR/debian/thunderbird/opt
+wget -nd -r -l1 --no-parent -A "*.bz2" http://releases.mozilla.org/pub/mozilla.org/thunderbird/releases/latest/linux-$ARCH_URL/en-US/
+mv *bz2 thunderbird.tar.bz2
+bzip2 -d thunderbird.tar.bz2
+tar xvf thunderbird.tar
+rm thunderbird.tar
 rm thunderbird/omni.ja
-cd ..
+rm -rf robots.txt
+cd $CURDIR
 
-echo "Downloaded thunderbird $RELEASE for $ARCHI"
-
-rm -rf opt/robots.txt
-
-export DEBFULLNAME='Clement Lefebvre'
-export DEBEMAIL='root@linuxmint.com'
-dch -v "$RELEASE" -b
-
-dpkg-buildpackage -us -uc -rfakeroot
 
 
